017d79120681bb80d99a70b44b53d800c71bf9c6
A.V. <92892055+7AV@users.noreply.github.com>
2023-11-26 14:22:55 -0500

    working

--- tracing.js
+++ tracing.js
@@ -1,41 +1,36 @@
+//OpenTelemetry
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
-// Instrumentations
+//exporter
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+//instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-// Exporter
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
-const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
+//Exporter
 module.exports = (serviceName) => {
-    // Create a Jaeger Exporter
-    const exporter = new JaegerExporter({
-        serviceName: serviceName,
-        host: '192.168.0.99', // Update this with the hostname or IP of your Jaeger instance
-        port: 6831, // Update this with the port number of your Jaeger instance
-    });
-
-    const provider = new NodeTracerProvider({
-        resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-        }),
-    });
-
-    provider.addSpanProcessor(new BatchSpanProcessor(exporter)); // Use BatchSpanProcessor instead
-    provider.register();
-
-    registerInstrumentations({
-        instrumentations: [
-            new HttpInstrumentation(),
-            new ExpressInstrumentation(),
-            new MongoDBInstrumentation(),
-        ],
-        tracerProvider: provider,
-    });
-
-    return trace.getTracer(serviceName);
+ const exporter = new JaegerExporter({
+   endpoint: "http://localhost:14268/api/traces",
+ });
+ const provider = new NodeTracerProvider({
+   resource: new Resource({
+     [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+   }),
+ });
+ provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+ provider.register();
+ registerInstrumentations({
+   instrumentations: [
+     new HttpInstrumentation(),
+     new ExpressInstrumentation(),
+     new MongoDBInstrumentation(),
+   ],
+   tracerProvider: provider,
+ });
+ return trace.getTracer(serviceName);
 };
